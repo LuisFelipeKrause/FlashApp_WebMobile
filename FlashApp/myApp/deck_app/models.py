ae568349d5e0352378654c67db50f5a17b042e36
LuisFelipeKrause <lfelipe.krause@gmail.com>
2024-05-17 00:58:49 -0300

    Reformulação. Tela reformulada. Versão estável sem funcionamento dos botões de editar, revisar e deletar

--- FlashApp/myApp/deck_app/models.py
+++ FlashApp/myApp/deck_app/models.py
@@ -8,6 +8,7 @@ class Deck(models.Model):
     descricao = models.CharField(max_length=300, blank=True, null=True)
     erros = models.IntegerField(default=0)
     acertos = models.IntegerField(default=0)
+    num_cards = models.IntegerField(default=0)
     desempenho_geral = models.FloatField(default=0.0)
     criado_em = models.DateTimeField(auto_created=True)
     ultima_revisao = models.DateTimeField(blank=True, null=True)
