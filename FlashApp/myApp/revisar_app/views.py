10b49acc549aaa42713db74d0134c3e3c5f569c9
LuisFelipeKrause <lfelipe.krause@gmail.com>
2024-06-10 15:33:45 -0300

    Versão mais avançada. Template de revisão já recebe todos os cards e o deck

--- FlashApp/myApp/revisar_app/views.py
+++ FlashApp/myApp/revisar_app/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from deck_app.models import Deck, Card
 
 # Create your views here.
-class Revisar(LoginRequiredMixin, View):
-    def get(self, request, pk):
-        return render(request, 'revisar_app/revisao.html')
+class RevisarCards(LoginRequiredMixin, View):
+    def get(self, request, pk):  # Usado para listar o card na revisão
+        deck = Deck.objects.filter(id=pk).get()
+        cards = Card.objects.filter(deck=pk)
+
+        contexto = {
+            'deck': deck,
+            'cards': cards
+        }
+        return render(request, 'revisar_app/revisao.html', contexto)
+
+    # def post()  # Usado para mandar o resultado da revisão
