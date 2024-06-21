1d97118cfe6965e51bb9a5d19479017fa0fcd97d
LuisFelipeKrause <lfelipe.krause@gmail.com>
2024-06-21 12:13:10 -0300

    Apresentação de estatísticas finalizada

--- FlashApp/myApp/revisar_app/views.py
+++ FlashApp/myApp/revisar_app/views.py
@@ -15,3 +15,23 @@ class RevisarCards(LoginRequiredMixin, View):
             'cards': cards,
         }
         return render(request, 'revisar_app/revisao.html', contexto)
+    
+class ExibirEstatisticas(LoginRequiredMixin, View):
+    def get(self, request, pk):
+        deck = Deck.objects.filter(id=pk).get()
+
+        total_cards = deck.acertos + deck.erros
+
+        if total_cards < 1:
+            total_cards = 1
+
+        acertos_porcentagem = (deck.acertos / total_cards) * 100
+        erros_porcentagem = (deck.erros / total_cards) * 100
+
+        contexto = {
+            'deck': deck,
+            'acertos': acertos_porcentagem,
+            'erros': erros_porcentagem,
+            'ultima_revisao': deck.ultima_revisao
+        }
+        return render(request, 'revisar_app/estatisticas.html', contexto)
